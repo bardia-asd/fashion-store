1b9d0c57bf35b769c787d98bafe03a329788c48a
Bardia Asadi <bardiaasd7@gmail.com>
2026-09-16 20:13:17 +0330

    chore: set up Redux Toolkit and React Redux

---
+++ src/app/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+const temporaryReducer = (state = {}) => state;
+
+export const store = configureStore({
+    reducer: temporaryReducer,
+});
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
     resolve: {
         alias: {
-            "@": path.resolve(__dirname, "./src"),
+            "@": `${import.meta.dirname}/src`,
         },
     },
 });

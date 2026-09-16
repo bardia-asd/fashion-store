01972a885a37383e819f823b0e217d54e62d7a49
Bardia Asadi <bardiaasd7@gmail.com>
2026-09-16 21:13:24 +0330

    feat: add button, input, label and separator components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "./components/ui/label";
+import { Separator } from "./components/ui/separator";
+
 const App = () => {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    return (
+        <div className="flex flex-col gap-4 p-5 max-w-lg">
+            <Label htmlFor="name">نام محصول:</Label>
+            <Input id="name" placeholder="نام محصول" className="h-11" />
+            <Separator />
+        </div>
+    );
 };
 
 export default App;
